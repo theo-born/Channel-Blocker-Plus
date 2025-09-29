92ff94b1964afe894b6311fd5860c2802addeed2
theo-born <theo.born999@gmail.com>
2025-09-29 00:02:13 -0400

    Update comment.js

--- content/observer_components/video/comment.js
+++ content/observer_components/video/comment.js
@@ -1,17 +1,17 @@
 const COMMENT_CONFIG = Object.freeze({
 	anchorSelector: ["ytd-comment-thread-renderer"],
 	characterDataSelectors: {
-		commentContent: ["yt-formatted-string#content-text"]
+		commentContent: ["yt-attributed-string#content-text"]
 	}
-});
+})
 
 const REPLY_COMMENT_CONFIG = Object.freeze({
-	anchorSelector: ["ytd-comment-renderer[is-reply]"],
+	anchorSelector: ["ytd-comment-view-model"],
 	characterDataSelectors: {
 		userChannelName: ["a#author-text", "span"],
-		commentContent: ["yt-formatted-string#content-text"]
+		commentContent: ["yt-attributed-string#content-text"]
 	}
-});
+})
 
 async function onCommentObserved(comment, characterDatas){
 	let authorText = $(comment).find("a#author-text")[0];
@@ -19,7 +19,11 @@ async function onCommentObserved(comment, characterDatas){
 
 	if(authorText.hidden){
 
-		userChannelName = $(comment).find("yt-formatted-string[class='style-scope ytd-channel-name']")[0].innerText.trim();
+		userChannelName = $(comment)
+			.find(
+				"span[class=' style-scope ytd-comment-view-model style-scope ytd-comment-view-model']"
+			)[0]
+			.innerText.trim()
 	}else{
 		userChannelName = $(authorText).find("span")[0].innerText.trim();
 	}
@@ -32,7 +36,9 @@ async function onCommentObserved(comment, characterDatas){
 }
 
 async function onReplyCommentObserved(replyComment, characterDatas){
-	let channelNameElem = $(replyComment).find("a#author-text")[0];
+	let channelNameElem = $(replyComment).find(
+		"span[class='yt-core-attributed-string yt-core-attributed-string--white-space-pre-wrap']"
+	)[0]
 
 	channelNameElem.style.display = "inline";
 	
